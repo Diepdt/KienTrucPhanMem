70feaf4261fb5355d66ed8590693816ce17d67e5
Diepdt <duongtuandiep2@gmail.com>
2026-01-18 16:04:06 +0700

    Hoàn thành bookstore với cấu trúc monolith

--- monolith/bookstore1/settings.py
+++ monolith/bookstore1/settings.py
@@ -121,7 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Khai báo cho Django biết tìm static file ở thư mục "monolith/static"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# 3. Khai báo thư mục gom file khi deploy (nếu cần sau này)
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
